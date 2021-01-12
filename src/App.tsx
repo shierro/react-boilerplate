17b8219f507d73da263347896fcc086fb0eb3540
Theo <theodoro.pelingan@gmail.com>
2021-01-12 15:35:19 +0800

    :sparkles: added 2 basic components(header, footer)

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header />
+      <div className="body">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+      </div>
+      <Footer />
     </div>
   );
 }
